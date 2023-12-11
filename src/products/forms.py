42edfdc5204f83927e7b871eb17b0487de92e531
Muhammadnihalc <nihalnazeer000@gmail.com>
2023-12-11 21:22:12 +0530

    Pagination, single view of product , requirement.txt

--- src/products/forms.py
+++ src/products/forms.py
@@ -1,5 +1,10 @@
-from flask_wtf.file import FileAllowed , FileField , FileRequired
-from wtforms import Form, BooleanField, StringField, PasswordField, validators , IntegerField , TextAreaField , DecimalField
+from flask_wtf.file import FileAllowed, FileField, FileRequired
+from wtforms import Form, StringField, validators, IntegerField, TextAreaField, DecimalField
+
+def images_only(form, field):
+    if field.data:
+        if not any(field.data.filename.lower().endswith(allowed_ext) for allowed_ext in ['jpg', 'png', 'jpeg', 'gif']):
+            raise validators.ValidationError('Images only.')
 
 class Addproducts(Form):
     name = StringField('Name', [validators.DataRequired()])
@@ -8,6 +13,6 @@ class Addproducts(Form):
     stock = IntegerField('Stock', [validators.DataRequired()])
     discription = TextAreaField('Discription', [validators.DataRequired()])
     colors = TextAreaField('colors', [validators.DataRequired()])
-    image_1 = FileField('Image 1', validators=[ FileAllowed(['jpg', 'png', 'jpeg', 'gif']) , 'images only'])
-    image_2 = FileField('Image 2', validators=[ FileAllowed(['jpg', 'png', 'jpeg', 'gif']) , 'images only'])
-    image_3 = FileField('Image 3', validators=[ FileAllowed(['jpg', 'png', 'jpeg', 'gif']) , 'images only'])
+    image_1 = FileField('Image 1', validators=[FileAllowed(['jpg', 'png', 'jpeg', 'gif']), images_only])
+    image_2 = FileField('Image 2', validators=[FileAllowed(['jpg', 'png', 'jpeg', 'gif']), images_only])
+    image_3 = FileField('Image 3', validators=[FileAllowed(['jpg', 'png', 'jpeg', 'gif']), images_only])
--- src/products/routes.py
+++ src/products/routes.py
@@ -7,27 +7,41 @@ from .forms import Addproducts
 
 @app.route('/')
 def home():
-    products = Addproduct.query.filter(Addproduct.stock > 0)
+    page = request.args.get('page',1,type=int)
+    products = Addproduct.query.filter(Addproduct.stock > 0).order_by(Addproduct.id.desc()).paginate(page=page , per_page = 4)
     brands = Brand.query.join(Addproduct, (Brand.id == Addproduct.brand_id)).all()
     categories = Category.query.join(Addproduct, (Category.id == Addproduct.category_id)).all()
     return render_template('products/index.html' , products=products , brands=brands , categories=categories)
 
+@app.route('/product/<int:id>')
+def single_page():
+    product = Addproduct.query.get_or_404(id)
+    brands = Brand.query.join(Addproduct, (Brand.id == Addproduct.brand_id)).all()
+    categories = Category.query.join(Addproduct, (Category.id == Addproduct.category_id)).all()
+    return render_template('products/single_page.html' , product=product , brands=brands , categories=categories)
+
+
 
 @app.route('/brand/<int:id>')
 def get_brand():
-    brand = Addproduct.query.filter(brand_id = id)
+    
+    page = request.args.get('page',1,type=int)
+    get_b = Brand.query.filter_by(id=id).first_or_404()
+    brand = Addproduct.query.filter(brand=get_b).paginate(page=page , per_page = 4)
     brands = Brand.query.join(Addproduct, (Brand.id == Addproduct.brand_id)).all()
     categories = Category.query.join(Addproduct, (Category.id == Addproduct.category_id)).all()
-    return render_template('products/index.html' , brand=brand , brands=brands , categories=categories)
+    return render_template('products/index.html' , brand=brand , brands=brands , categories=categories , get_b=get_b)
 
 
 
 @app.route('/categories/<int:id>')
 def get_category():
-    get_prod= Addproduct.query.filter(category_id = id)
+    page = request.args.get('page',1,type=int)
+    get_cat = Category.query.filter_by(id=id).first_or_404()
+    get_prod= Addproduct.query.filter_by(category= get_cat).paginate(page=page , per_page = 4)
     categories = Category.query.join(Addproduct, (Category.id == Addproduct.category_id)).all()
     brands = Brand.query.join(Addproduct, (Brand.id == Addproduct.brand_id)).all()
-    return render_template('products/index.html' , get_prod=get_prod , categories=categories, brands=brands)
+    return render_template('products/index.html' , get_prod=get_prod , categories=categories, brands=brands , get_cat=get_cat)
 
 
 
