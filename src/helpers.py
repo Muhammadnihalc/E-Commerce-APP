9000f856a2ba7ba41156431dae6de0f950213b02
Muhammadnihalc <89681779+Muhammadnihalc@users.noreply.github.com>
2023-12-20 12:39:34 +0530

    Updating helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -1,11 +1,14 @@
 import os
 from werkzeug.utils import secure_filename
 from flask import current_app
+import secrets  
 
 # function which will help to save the uploaded images in static/images folder
-def save_image(image, upload_folder='static/images'):
+def save_image(image, upload_folder='static/images', name=None):
     if image:
         filename = secure_filename(image.filename)
+        if name:
+            filename = name + filename
         filepath = os.path.join(current_app.root_path, upload_folder, filename)
         image.save(filepath)
         return filename
